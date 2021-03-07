0c866cba06a1d9f4fac5a6344b137cc0d2d417de
raphaelsalak <61638085+raphaelsalak@users.noreply.github.com>
2021-03-07 14:08:31 -0600

    deleted some stuff

--- steveharveyproject/src/Main.java
+++ steveharveyproject/src/Main.java
@@ -1,8 +1,7 @@
- package steveharveyproject;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World This is real");
 		// TODO Auto-generated method stub
 
 	}
